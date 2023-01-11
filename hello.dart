a0ed26d1c5d0e7daf46fc66be0611a1bcb180581
Om Bhojane <82753658+ombhojane@users.noreply.github.com>
2023-01-11 20:25:12 +0530

    Create hello.dart
    Day 2: Creating a basic Hello world app

---
+++ hello.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello'),
+          centerTitle: true,
+        ),
+        body: Center(child: Text('Hello')),
+        
+      ),
+    ));
