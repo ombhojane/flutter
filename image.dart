52efc6eb3b385f6c6e4bc9d9f87cd04e8bb6a1f1
Om Bhojane <82753658+ombhojane@users.noreply.github.com>
2023-01-12 05:53:04 +0530

    Create image.dart
    uploaded image file in app

---
+++ image.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Image.network(
+            'https://images.unsplash.com/photo-1673427303785-2f077047aac3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (() {}),
+        child: Text('button'),
+        backgroundColor: Colors.yellow,
+      ),
+    );
+  }
+}
